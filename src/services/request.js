aad4a9bc63cc363c4dc05418ae428862607523e4
Branislav Djuric <brani.djuric@gmail.com>
2018-01-03 15:58:44 +0100

    WalletServices: checkBalance & sendETH part

--- src/services/request.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const client = axios.create({
-  baseURL: 'https://celsiusnetwork.auth0.com/oauth/token',
-  clientId: 'LOHU3qeHAxFUE34Q71bfMUtdHW7afyLl'
-})
-
-export default
