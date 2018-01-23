5238959ad389b417bd6e23fc1660a4323d311cbd
Djordje Stevanovic <dsdjolence@gmail.com>
2018-01-23 13:37:37 +0100

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -2,81 +2,61 @@ import React from 'react'
 import { Container } from 'native-base'
 import { Provider } from 'react-redux'
 import { applyMiddleware, createStore } from 'redux'
-import { View, Text, Image } from 'react-native';
+import { Image } from 'react-native'
 import thunk from 'redux-thunk'
-// import { Font } from 'expo'
-import { Font, AppLoading, Asset } from "expo";
+import { Font, AppLoading, Asset } from 'expo'
 
 import reducers from './reducers/index'
 import Navigator from './Navigator'
 
-import { composeWithDevTools } from 'remote-redux-devtools';
+import { composeWithDevTools } from 'remote-redux-devtools'
 
 const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(thunk)))
 
-function cacheImages(images) {
+function cacheImages (images) {
   return images.map(image => {
     if (typeof image === 'string') {
-      return Image.prefetch(image);
+      return Image.prefetch(image)
     } else {
-      return Asset.fromModule(image).downloadAsync();
+      return Asset.fromModule(image).downloadAsync()
     }
-  });
-}
-
-function cacheFonts(fonts) {
-  return fonts.map(font => Font.loadAsync(font));
+  })
 }
 
 class App extends React.Component {
-  state = {
-    isReady: false,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = { loading: true };
+  constructor () {
+    super()
+    this.state = { loading: true }
   }
 
-  async _loadAssetsAsync() {
+  static async loadAssetsAsync () {
     const imageAssets = cacheImages([
       require('../assets/images/background.png'),
-      require('../assets/images/background-blur.png'),
-    ]);
+      require('../assets/images/background-blur.png')
+    ])
 
-    // const fontAssets = cacheFonts([FontAwesome.font]);
-
-    await Promise.all([...imageAssets]);
+    await Promise.all([...imageAssets])
   }
 
-  async componentWillMount() {
+  async componentWillMount () {
     await Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
-    });
-
-    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
       'barlow-medium': require('../assets/fonts/Barlow-Medium.ttf')
     })
-    this.setState({ loading: false });
-  }
 
-  getState = () => {
-    store.getState()
-  }
-
-  componentWillReceiveProps () {
+    this.setState({ loading: false })
   }
 
   render () {
     if (!this.state.isReady) {
       return (
         <AppLoading
-          startAsync={this._loadAssetsAsync}
+          startAsync={App.loadAssetsAsync}
           onFinish={() => this.setState({ isReady: true })}
           onError={console.warn}
         />
-      );
+      )
     }
 
     return (
