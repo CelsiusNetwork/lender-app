8d7bbd0efad98ea53ea7de428c1e52c539fb3094
Djordje Stevanovic <dsdjolence@gmail.com>
2018-01-27 01:48:05 +0100

    Refactoring: Plop (service update template)

--- plops/service/serviceGen.js
+++ plops/service/serviceGen.js
@@ -5,6 +5,11 @@ module.exports = {
       type: 'input',
       name: 'name',
       message: 'What is your service name? (eg. ethereum)'
+    },
+    {
+      type: 'input',
+      name: 'path',
+      message: 'What is the path to the endpoint for your service? (eg. /balance)'
     }
   ],
   actions: [
