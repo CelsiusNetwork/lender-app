52ad85fa15f8b66952fbba8edb0db356eae7bf70
Djordje Stevanovic <dsdjolence@gmail.com>
2018-01-29 14:48:32 +0100

    Refactoring: Wallet Balance

--- src/reducers/WalletBalance.js
+++ src/reducers/WalletBalance.js
@@ -10,20 +10,22 @@ const LENDER_WALLET_STATE = {
 export default (state = LENDER_WALLET_STATE, action) => {
   switch (action.type) {
     case types.FETCH_WALLET_BALANCE_LOADING:
-      return { ...state, loading: true }
+      return {...state, loading: true}
+
     case types.FETCH_WALLET_BALANCE_SUCCESS:
-      const a = action.payload
+      const payload = action.payload
       return {
         ...state,
-        ethBalance: a.ether.toFixed(2),
-        celBalance: a.deg.toFixed(2),
+        ethBalance: payload.ether.toFixed(2),
+        celBalance: payload.deg.toFixed(2),
         loading: false,
         error: ''
       }
+
     case types.FETCH_WALLET_BALANCE_ERROR:
-      let errorMsg
-      errorMsg = 'Wallet balance error'
-      return { ...state, error: errorMsg, loading: false }
+      let errorMsg = 'Wallet balance error'
+      return {...state, error: errorMsg, loading: false}
+
     default:
       return state
   }
