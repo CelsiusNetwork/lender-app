57980a0feb37ac8c46f91b8a38409a5a9e759d91
fj <filip.jovakaric@gmail.com>
2018-01-30 11:54:01 +0100

    CN-30: refactoring HistoryDetail

---
+++ src/components/HistoryDetail/HistoryDetail.styles.js
@@ -0,0 +1,96 @@
+export default {
+  aCenter: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  ethWrapper: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 38,
+    position: 'relative'
+  },
+  eth: {
+    marginTop: 35,
+    width: 80,
+    height: 80,
+    resizeMode: 'contain'
+  },
+  text: {
+    fontSize: 14
+  },
+  button: {
+    marginRight: 30,
+    marginLeft: 30,
+    width: 150,
+    marginTop: 40
+  },
+  circle: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    borderColor: '#27517B',
+    borderWidth: 2,
+    alignItems: 'center'
+  },
+  green: {
+    backgroundColor: '#47CA53'
+  },
+  red: {
+    backgroundColor: '#ff3333'
+  },
+  icon: {
+    marginTop: 10,
+    width: 22,
+    height: 22,
+    resizeMode: 'contain'
+  },
+  points: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+    marginTop: 1,
+    marginLeft: 5
+  },
+  row: {
+    height: 50,
+    borderTopColor: 'rgba(255, 255, 255, 0.1)',
+    borderTopWidth: 2,
+    marginRight: 8,
+    marginLeft: 8,
+    borderBottomColor: 'rgba(255, 255, 255, 0.1)',
+    borderBottomWidth: 2
+  },
+  cellLeft: {
+    marginLeft: 10
+  },
+  cellLeftText: {
+    color: 'rgba(255, 255, 255, 0.5)',
+    fontSize: 21,
+    fontFamily: 'barlow'
+  },
+  cellRight: {
+    marginRight: 10,
+    paddingLeft: 20
+  },
+  cellRightText: {
+    color: 'rgba(255, 255, 255, 1)',
+    fontSize: 21,
+    textAlign: 'left',
+    fontFamily: 'barlow-semi-bold'
+  },
+  greenDow: {
+    width: 10,
+    height: 10,
+    backgroundColor: '#47CA53',
+    borderRadius: 5,
+    marginTop: 7,
+    marginRight: 5
+  }
+}
